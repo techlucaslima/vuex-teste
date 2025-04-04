8de344fe47baa93d8b845d24df71ee8f7cc93797
Lucas Lima <mosukasangamer@gmail.com>
2025-04-04 20:37:14 -0300

    Mecânica simples de adicionar ao carrinho usando vuex

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div> 
+        <h2>Banana</h2>
+        <button @click="addToCart({nome: 'banana', preco: 12, por: 'kg'})">Adicionar ao carrinho</button>
+        <h2>Itens no carrinho: {{ cartItemCount }}</h2>
+        <br>
+        <div v-for="(item, index) in cart" :key="index">
+            <h2>{{ item.nome }} - R${{ item.preco }} por {{ item.por }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { mapState, mapActions, mapGetters } from 'vuex';
+
+    export default {
+        computed: {
+            ...mapState(['cart']),
+            ...mapGetters(['cartItemCount']),
+        },
+        methods: {
+            ...mapActions(['addToCart']),
+        },
+    };
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        cart: [],
+    },
+    mutations: {
+        ADD_TO_CART(state, product) {
+            state.cart.push(product);
+        },
+    },
+    actions: {
+        addToCart({ commit }, product) {
+            commit('ADD_TO_CART', product);
+        },
+    },
+    getters: {
+        cartItemCount: state => {
+            return state.cart.length;
+        },
+    },
+});
